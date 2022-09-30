ed2dcdef8a96ca4e5fedd6021b0902a0731373de
GuidoBergmans <75182919+GuidoBergmans@users.noreply.github.com>
2022-09-30 22:01:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,49 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    move(up)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    move(left)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    move(right)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    move(down)
+})
+function move (direction: number) {
+    if (direction == up) {
+        for (let x = 0; x <= cells_x; x++) {
+            for (let y = 0; y <= cells_y; y++) {
+                if (list[0] == 0) {
+                	
+                }
+            }
+        }
+    } else if (direction == down) {
+    	
+    } else if (direction == left) {
+    	
+    } else {
+    	
+    }
+}
+let list: number[] = []
+let right = 0
+let left = 0
+let down = 0
+let up = 0
+let cells_y = 0
+let cells_x = 0
 scene.setBackgroundColor(13)
-let cells_x = 4
-let cells_y = 4
+cells_x = 4
+cells_y = 4
 let cells = cells_x * cells_y
 let max_cell_index = cells - 1
-let list = [cells_x, cells_y]
+up = 0
+down = 1
+left = 2
+right = 3
+list = [cells_x, cells_y]
 for (let index = 0; index <= max_cell_index; index++) {
     let empty_cell = 0
     list[index] = empty_cell
