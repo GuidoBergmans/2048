a5ef7ec9b2b393ec7ac70051b8d7442f6cf024ff
GuidoBergmans <75182919+GuidoBergmans@users.noreply.github.com>
2022-10-01 18:54:03 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
+namespace SpriteKind {
+    export const tile = SpriteKind.create()
+}
+function DrawScreen () {
+    for (let index = 0; index <= max_cell_index; index++) {
+    	
+    }
+    for (let y = 0; y <= y - 1; y++) {
+        for (let x = 0; x <= cells_x - 1; x++) {
+            mySprite = sprites.create(img`
+                ............................
+                .444444444444444444444444444
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .4.........................4
+                .444444444444444444444444444
+                `, SpriteKind.tile)
+            mySprite.left = x * mySprite.width
+            mySprite.top = y * mySprite.height
+            mySprite.sayText(2 ** index)
+            index = index + 1
+            tiles2.push(mySprite)
+        }
+    }
+    index = 0
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     move(up)
 })
@@ -27,19 +75,23 @@ function move (direction: number) {
     	
     }
 }
+let index = 0
+let mySprite: Sprite = null
+let tiles2: Sprite[] = []
 let list: number[] = []
 let right = 0
 let left = 0
 let down = 0
 let up = 0
+let max_cell_index = 0
 let cells_y = 0
 let cells_x = 0
 scene.setBackgroundColor(13)
 cells_x = 4
 cells_y = 4
 let cells = cells_x * cells_y
-let max_cell_index = cells - 1
-up = 0
+max_cell_index = cells - 1
+up = scene.screenHeight()
 down = 1
 left = 2
 right = 3
@@ -48,4 +100,13 @@ for (let index = 0; index <= max_cell_index; index++) {
     let empty_cell = 0
     list[index] = empty_cell
 }
-list[randint(0, max_cell_index)] = randint(1, 2) * 2
+let index1 = randint(0, max_cell_index)
+let index2 = randint(0, max_cell_index - 1)
+if (index2 >= index1) {
+    index2 = index2 + 1
+}
+list[index1] = randint(1, 2) * 2
+list[index2] = randint(1, 2) * 2
+tiles2 = sprites.allOfKind(SpriteKind.tile)
+scene.setBackgroundColor(1)
+DrawScreen()
